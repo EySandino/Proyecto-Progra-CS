4bc13c8b57397194b977c075627e788ab328d52f
Eyder Sandino <eyder.sandino@gmail.com>
2023-03-16 00:51:05 -0600

    primera prueba de conexion

---
+++ src/main/main.java
@@ -0,0 +1,9 @@
+package main;
+
+import ControlBaseDatos.ControlVentas;
+
+public class main {
+    public static void main(String[] args){
+        ControlVentas connection = new ControlVentas();
+    }
+}
